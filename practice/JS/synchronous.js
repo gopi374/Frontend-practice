f5a85cd17628b023b534152cd273cac9e206f4e1
gopi374 <an3201088@gmail.com>
2026-06-23 12:35:40 +0530

    Interview Q/A

--- practice/JS/synchronous.js
+++ practice/JS/synchronous.js
@@ -31,32 +31,46 @@ function wait(){
 
 
 
-let c = 0;
-const i = setInterval(()=>{
-    c++;
-    console.log(c);
-    if(c==3){
-        clearInterval(i);
-    }
-},10000)
+// let c = 0;
+// const interval = setInterval(()=>{
+//     c++;
+//     console.log(c);
+//     if(c==3){
+//         clearInterval(interval);
+//     }
+// },10000)
 
-setTimeout(() => {
-    console.log('timeout')
-}, 2500); 
+// setTimeout(() => {
+//     console.log('timeout')
+// }, 2500); 
 
 
 //imp interview Q/A
-for(let i=1;i<4;i++){
-    setTimeout(()=>{
-        console.log(i)
-    },i*1000)
-} //o/p-1 2 3
+// for(let i=1;i<4;i++){
+//     setTimeout(()=>{
+//         console.log(i)
+//     },i*1000)
+// } //o/p-1 2 3
 
-for(var i=1;i<4;i++){
-    setTimeout(()=>{
-        console.log(i)
-    },i*1000)
-} // o/p - 4 4 4
+// for(var i=1;i<4;i++){
+//     setTimeout(()=>{
+//         console.log(i)
+//     },i*1000)
+// } // o/p - 4 4 4
 
 // because fo scoping - var is function & global scope , it points to single i=4;
 // let is block scope , it points different i with every timeout -1 2 3 
+
+
+
+let count = 3;
+const counter = setInterval(() => {
+    if(count>0){
+        console.log(count)
+        count--;
+    }
+    else{
+        console.log("ended")
+        clearInterval(counter)
+    }
+}, 1000);
