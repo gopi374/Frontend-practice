6b821b953389e3430ff1137009eb46926dee9ef5
gopi374 <an3201088@gmail.com>
2026-06-23 14:43:55 +0530

    CallBacks()

--- practice/JS/callbacks.js
+++ practice/JS/callbacks.js
@@ -1,5 +1,5 @@
 //callbacks - A callback is a function that is passed as an argument to another function and is executed later.
-// it helps to set the order of execution of function
+// it helps to set or handle sequential order of execution of function.
 
 function maggielana(cb){
     console.log("maggie lene gye ...")
@@ -24,6 +24,11 @@ function maggiekhana(){
         console.log("maggie khatam...")
     })
 }
-
+// callbaks workflow
+// maggielana(()=>{
+//     maggiebanana(()=>{
+//         maggiekhana(()=>{});
+//     });
+// });
 
 maggielana(maggiebanana);
