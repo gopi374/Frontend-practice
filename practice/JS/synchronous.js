725fec85d4dd787f15222a1109363a1aba462214
gopi374 <an3201088@gmail.com>
2026-06-23 11:46:31 +0530

    setTimeOut(),setInterval(), clearInterval()

--- practice/JS/synchronous.js
+++ practice/JS/synchronous.js
@@ -2,11 +2,23 @@ function wait(){
     // let time = new Date().getTime();
     // while(new Date().getTime()<time+5000){}
 
+    // setTimeout(()=>{
+    //     console.log("after 5 sec")
+    // },5000);
+    let id = setInterval(() => {
+        console.log("4.9 sec")
+    }, 4900);
+
     setTimeout(()=>{
-        console.log("after 5 sec")
-    },5000);
+    clearInterval(id)
+    console.log(`interval cleared in 10s`)
+    },10000)
+
 }
+
 
 console.log('hlello')
 wait();
 console.log("worldd")
+
+// console.log(new Date().getMinutes())
