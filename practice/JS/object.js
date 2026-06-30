e567497f61bcf749f98daa5bbe54537569a39221
gopi374 <an3201088@gmail.com>
2026-06-30 12:44:58 +0530

    OOPS

---
+++ practice/JS/object.js
@@ -0,0 +1,30 @@
+// const student1={
+//     name : "Gopi",
+//     attendance : 50,
+
+//     ispresent:function(){
+//         this.attendance++;
+//         console.log(`${this.name} is present. Total attendance ${this.attendance}`)
+//     }
+// }
+// student1.ispresent()
+
+
+
+class Student{
+    constructor(name,att){
+        this.name=name;
+        this.att=att;
+    }
+
+    ispresent(){
+        this.att++;
+        console.log(`${this.name} is present. total Attendence ${this.att}`)
+    }
+    static greet(){//only class can access this function
+        console.log("hello user")
+    }
+}
+const std1=new Student("Gopi",50);
+Student.greet();
+std1.ispresent();
