591eb095f8ca1d9d3893e68f6d48998057bb56b0
gopi374 <an3201088@gmail.com>
2026-07-02 16:07:44 +0530

    EventListeners

--- practice/JS/DOM/EventListners.js
+++ practice/JS/DOM/EventListners.js
@@ -1,2 +1,9 @@
 const btn  =  document.querySelector("#btn")
-
+let i=0;
+// btn.onclick=()=>{
+//     alert("pressed")
+//     console.log(i++)
+// }
+addEventListener("dblclick",()=>{
+    alert("dbl clicked ho gya")
+})
