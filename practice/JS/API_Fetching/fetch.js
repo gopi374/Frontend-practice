b87d295aa70fdfbc7a1462c07cdb2ef2c9daef68
gopi374 <an3201088@gmail.com>
2026-07-10 23:52:34 +0530

    API fetching

--- practice/JS/API_Fetching/fetch.js
+++ practice/JS/API_Fetching/fetch.js
@@ -1,13 +1,22 @@
 let url = "https://jsonplaceholder.typicode.com/todos";
 
+const btn = document.querySelector("#btn");
 const ul = document.querySelector("#list");
 
-fetch(url)
-  .then((data) => {
-    return data.json();
-  })
-  .then((data) => {
-    for (let i = 0; i < data.length; i++) {
-      console.log(data[i]);
-    }
-  });
+btn.addEventListener("click", () => {
+  fetch(url)
+    .then((res) => res.json())
+    .then((data) => {
+      ul.innerHTML = "";
+      // show first 10 items
+      data.slice(0, 10).forEach((item) => {
+        const li = document.createElement("li");
+        li.textContent = `${item.id}. ${item.title} [${item.completed ? "done" : "todo"}]`;
+        ul.appendChild(li);
+      });
+    })
+    .catch((err) => {
+      console.error(err);
+      ul.innerHTML = "<li>Error loading data</li>";
+    });
+});
