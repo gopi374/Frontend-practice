1007815c27bfe20f7023c1ababbbb77ce28f7f5b
gopi374 <an3201088@gmail.com>
2026-07-10 23:20:12 +0530

    API fetching

--- practice/JS/DOM/EventListners.js
+++ practice/JS/DOM/EventListners.js
@@ -1,9 +1,28 @@
 const btn  =  document.querySelector("#btn")
 let i=0;
 // btn.onclick=()=>{
-//     alert("pressed")
+//     alert("first event")
 //     console.log(i++)
-// }
-addEventListener("dblclick",()=>{
-    alert("dbl clicked ho gya")
+// } 
+// btn.onclick=()=>{
+//     alert("second event")
+//     console.log(i++)
+// } //only this event run , previous one is overridden
+
+function hello(){
+    console.log("hello ")
+}
+function hello1(){
+    alert("hello ")
+}
+// using addEventListener we can set multiple event on a single tag or thing
+btn.addEventListener("dblclick",hello);
+addEventListener()
+
+// btn.addEventListener("click",hello1)
+
+
+const inp  =  document.querySelector("#inp")
+inp.addEventListener("keypress",()=>{
+    console.log("keyprocess")
 })
