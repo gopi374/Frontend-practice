bb49ed4d7e1486ea8a3dab28633a035df2862eb5
gopi374 <an3201088@gmail.com>
2026-07-25 23:48:12 +0530

    Passport js

--- 12_JWT_SESSION_COOKIES/app.js
+++ 12_JWT_SESSION_COOKIES/app.js
@@ -1,16 +1,9 @@
 const jwt = require("jsonwebtoken");
 const express = require("express");
-// const mongoose = require("mongoose");
 
 const app = express();
 
 app.use(express.json());
-
-
-// mongoose
-//   .connect("mongodb://localhost:27012/jwt")
-//   .then(() => console.log("✅ Mongodb is connected !"))
-//   .catch((err) => console.log(err));
 
 
 const SECRET_KEY = "my_super_secret_key";
@@ -27,6 +20,15 @@ app.post("/api/login", (req, res) => {
 
 });
 
+app.post("/api/get-token",(req,res)=>{
+
+  const token = jwt.sign("this is my msg",SECRET_KEY,{algorithm:"HS256",issuer:"Gopi"})
+  res.status(200).json({
+    msg:"geting ",
+    token:token,
+  })
+
+})
 
 app.get("/api/profile", (req, res) => {
   // The client sends the token in the headers (Authorization: Bearer <token>)
@@ -53,6 +55,60 @@ app.get("/api/profile", (req, res) => {
     });
   });
 });
+
+
+app.get("/check-token", (req, res) => {
+  let { token } = req.query;
+  try {
+    let data = jwt.verify(token, JWT_SECRET);
+
+    res.send({
+      msg: "Token is genuine",
+      data,
+    });
+  } catch (error) {
+    res.send({
+      msg: "Token ke saath chedh khaani kari gai hai",
+      err: error.message,
+    });
+  }
+});
+
+app.post("/",async (req,res)=>{
+  let {token}=req.query;
+  try{
+    let data=jwt.verify(token,JWT_SECRET);
+
+    res.send({
+      msg:"token verified",
+      data,
+    });
+  }catch(error){
+    res.send({
+      msg:"invalid token",
+      error:error.message,
+    })
+  }
+})
+
+app.get(
+  "/delete-database",
+  function (req, res, next) {
+    let { token } = req.query;
+    let data = jwt.verify(token, JWT_SECRET);
+
+    if (data.superAdmin) return next();
+    else
+      res.status(404).send({
+        msg: "Invalid request",
+      });
+  },
+  (req, res) => {
+    res.send({
+      msg: "Poora Data base udda diya",
+    });
+  },
+);
 
 
 app.listen(4000, () => {
--- 12_JWT_SESSION_COOKIES/cookies.js
+++ 12_JWT_SESSION_COOKIES/cookies.js
@@ -18,32 +18,34 @@ mongoose
   })
   .catch((err) => console.log(err));
 
+
+
 const userSchema = mongoose.Schema({
   email: String,
   password: String,
 });
 const User = mongoose.model("user", userSchema);
 
-app.post("/signup", async (req, res) => {
-  let {email,password} = req.body;
-  let user = await User.findOne({email});
-  console.log(user);
-  if(user){
-    return res.status(400).json({
-        msg : "email is already exists !"
-    });
-  }
+app.post("/signup",async (req,res)=>{
+    let {email,password}=req.body;
 
+    let user =await User.findOne({email});
+    console.log(user)
+    if(user){
+        return res.status(404).send({
+            msg :"Already email in use !",
+        })
+    }
+    
     await User.create({
-      email,
-      password,
+        email,
+        password,
     })
 
-    res.send({
-        msg:"user signup success"
-    });
-});
-
+    res.status(201).send({
+        msg:"Account registered successfully"
+    })
+})
 
 app.post("/login",async (req,res)=>{
     const {email,password} = req.body;
