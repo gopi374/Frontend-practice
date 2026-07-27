6365006c92d9aac5df68427b9a52ca88865f0234
gopi374 <an3201088@gmail.com>
2026-07-27 15:29:45 +0530

    ReactjS basics

--- 14_ReactJS/1_React_basics/src/main.jsx
+++ 14_ReactJS/1_React_basics/src/main.jsx
@@ -1,10 +1,26 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+/* eslint-disable react-refresh/only-export-components */
+// import { StrictMode } from 'react';
+import { createRoot } from 'react-dom/client';
+import './index.css';
+// import App from './App.jsx'
+
+let todos = [
+  { name: 'Swim', des: ' is good for health' },
+  { name: 'Swim', des: ' is good for health' },
+];
+
+function App({ todos }) {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <li>
+          {todo.name}:{todo.des}
+        </li>
+      ))}
+    </ul>
+  );
+}
 
 createRoot(document.getElementById('root')).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+  <App todos={todos} />
+);
