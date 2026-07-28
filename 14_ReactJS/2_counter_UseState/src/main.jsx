f945139c9a6a7d5e6fd3abaed420ade58aa42c38
gopi374 <an3201088@gmail.com>
2026-07-29 00:31:13 +0530

    React hooks, todo app

--- 14_ReactJS/2_counter_UseState/src/main.jsx
+++ 14_ReactJS/2_counter_UseState/src/main.jsx
@@ -4,6 +4,7 @@ import './index.css'
 import App from './App.jsx'
 
 createRoot(document.getElementById('root')).render(
+  //StrinctMode is used to debug the error before render
   <StrictMode>
     <App />
   </StrictMode>
