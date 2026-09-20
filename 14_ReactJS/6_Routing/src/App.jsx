909ac2bdcbd19233543316d05ec07d020c20b1d6
gopi374 <an3201088@gmail.com>
2026-09-20 20:33:05 +0530

    Routing

--- 14_ReactJS/6_Routing/src/App.jsx
+++ 14_ReactJS/6_Routing/src/App.jsx
@@ -1,29 +1,23 @@
-import Home from "./Home"
-import About from "./About"
-import Notfound from "./NotFound"
-import {BrowserRouter,Routes,Route,Link} from "react-router-dom"
+import { Route, Routes } from 'react-router-dom'
+import Home from './pages/Home'
+import About from './pages/About'
+import NoteFound from './pages/NotFound'
+
 const App = () => {
   return (
     <div>
-
-      <BrowserRouter>
-      <nav>
-        <div>
-          <Link to="/">Home</Link>
+      <div className="navbar">
+        <h1>YOLO APP</h1>
+        <div className='a'>
+          <a href="">hello</a><a href="">hello</a><a href="">hello</a><a href="">hello</a><a href="">hello</a>
         </div>
-        <div>
-          <Link to="/about">About</Link>
-        </div>
-      </nav>
-        <Routes>
-          <Route path='/' element={<Home/>}/>
-          <Route path='/about' element={<About/>}/>
-          <Route path='/*' element={<Notfound/>}/>
+      </div>
 
-          
-        </Routes>
-      </BrowserRouter>
-      
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='/about' element={<About/>} />
+        <Route path='*' element={<NoteFound/>} />
+      </Routes>
     </div>
   )
 }
